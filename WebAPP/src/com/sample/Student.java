bf2fe46433a02964ef88d51121a7d3808637e797
msn9110 <msn9110@gmail.com>
2017-05-07 11:24:32 +0800

    hadoop example

---
+++ WebAPP/src/com/sample/Student.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class Student {
+	String id;
+	String name;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setID(String id){
+		this.id = id;
+	}
+}
